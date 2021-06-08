a9473ec6cf41cc72b083c4ba83a6147627fd7eaa
shun-harutaro <n.shunshun2004@gmail.com>
2021-06-08 20:59:36 +0900

    solve problems

--- hosting/nuxt.config.js
+++ hosting/nuxt.config.js
@@ -2,8 +2,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  /*
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'static'
+  */
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
