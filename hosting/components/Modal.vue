c09b4c4fddc1d40b39e096f2d5be3c0a8b779ee1
shun-harutaro <n.shunshun2004@gmail.com>
2021-06-10 22:12:57 +0900

    delete style tag

--- hosting/components/Modal.vue
+++ hosting/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="modal fixed inset-0">
-    <div class="modal-wrap relative">
+  <div class="w-full h-full fixed inset-0">
+    <div class="w-full h-full bg-black bg-opacity-50 modal-wrap relative">
       <!-- このボタンを押すとカスタムイベントcloseModalが発生 -->
       <div class="bg-white w-2/5 h-40 absolute m-auto inset-0">
         <button @click="$emit('closeModal')">
@@ -10,15 +10,3 @@
     </div>
   </div>
 </template>
-
-<style>
-.modal {
-  width: 100%;
-  height: 100%;
-}
-.modal-wrap {
-  background-color: rgba(0, 0, 0, 0.5);
-  width: 100%;
-  height: 100%;
-}
-</style>
