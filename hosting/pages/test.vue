9e64f240263a99ea94bf4f31466341ff5fc98b28
shun-harutaro <n.shunshun2004@gmail.com>
2021-06-19 21:49:02 +0900

    #62 emit data transfer

---
+++ hosting/pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello from App.vue</h1>
+    <p>parent_num: {{ parent_num }}</p>
+    <Scan @my-click='parent_num = $event'/>
+  </div>
+</template>
+
+<script>
+import Scan from '~/components/Scan.vue'
+export default {
+  data() {
+    return {
+      parent_num: 100
+    }
+  },
+  components: {
+    Scan
+  }
+}
+</script>
